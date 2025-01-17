35eb875371445f000ccf504f2d4965736089cfa1
Osmig_Torres <torresosmig@gmail.com>
2025-01-17 09:34:36 -0800

    homescreen

---
+++ AccountingApp/src/screens/HomeScreen.java
@@ -0,0 +1,10 @@
+package screens;
+
+public class HomeScreen {
+
+    public static void ShowHomeScreen() {
+        System.out.println("Welcome to Finacial Tracker");
+        System.out.println("What would you like to do?: ");
+        System.out.println("Deposit(D), Make a Payment(P), Display ledger(L), Exit(E) ");
+    }
+}
