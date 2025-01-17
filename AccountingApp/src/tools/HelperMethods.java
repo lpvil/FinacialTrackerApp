1b45e367d634ead27c0c189970287e44ca430f93
Osmig_Torres <torresosmig@gmail.com>
2025-01-17 10:17:13 -0800

    deleted things

--- AccountingApp/src/tools/HelperMethods.java
+++ AccountingApp/src/tools/HelperMethods.java
@@ -1,5 +1,12 @@
 package tools;
 
-public class HelperMethods {
+import java.util.Scanner;
 
+public class HelperMethods {
+    public static String getUserAnswer(String q){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("You: ");
+        String answer = scanner.nextLine();
+        return answer;
+    }
 }
